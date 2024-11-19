1541406c812495b4783a89205428d9dc8cdcd649
maxProMax <thisismaxproemail@gmail.com>
2024-11-19 18:04:31 +0100

    gtm

--- src/components/cookie-consent-banner/Banner.tsx
+++ src/components/cookie-consent-banner/Banner.tsx
@@ -7,71 +7,117 @@ import { useTranslations } from "next-intl";
 function gtag(...args: unknown[]) {
   window.dataLayer?.push(args);
 }
-function allConsentGranted() {
-  gtag("consent", "update", {
-    ad_user_data: "granted",
-    ad_personalization: "granted",
-    ad_storage: "granted",
-    analytics_storage: "granted",
-  });
-  gtag("js", new Date());
-  gtag("config", `${process.env.NEXT_PUBLIC_GMT_ID}`);
-}
-
-const gtmScript = ` 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-
-  gtag(\`consent\`, \`default\`, {
-    ad_storage: \`denied\`,
-    ad_user_data: \`denied\`,
-    ad_personalization: \`denied\`,
-    analytics_storage: \`denied\`,
-    wait_for_update: 500
-  });
-  `;
+// function allConsentGranted() {
+//   const data = {
+//     ad_storage: "granted",
+//     ad_user_data: "granted",
+//     analytics_storage: "granted",
+//     ad_personalization: "granted",
+//     personalization_storage: "granted",
+//     functionality_storage: "granted",
+//     security_storage: "granted",
+//   };
+//   localStorage.setItem("consentMode", JSON.stringify(data));
+//   gtag("consent", "update", data);
+//   // gtag("js", new Date());
+//   // gtag("config", `${process.env.NEXT_PUBLIC_GMT_ID}`);
+// }
 
 export const CookieConsentBanner = () => {
   const t = useTranslations();
 
   const [state, updateState] = useState<{
-    cookieVal?: boolean;
     showBanner: boolean;
   }>({ showBanner: false });
 
   const handleAccept = () => {
-    localStorage.setItem("cookieConsent", "true");
-    allConsentGranted();
-    updateState({ cookieVal: true, showBanner: false });
+    const data = {
+      ad_storage: "granted",
+      ad_user_data: "granted",
+      analytics_storage: "granted",
+      ad_personalization: "granted",
+      personalization_storage: "granted",
+      functionality_storage: "granted",
+      security_storage: "granted",
+    };
+    localStorage.setItem("consentMode", JSON.stringify(data));
+    gtag("consent", "update", data);
+    updateState({ showBanner: false });
   };
 
   const handleDecline = () => {
-    localStorage.setItem("cookieConsent", "false");
-    updateState({ cookieVal: false, showBanner: false });
+    const data = {
+      ad_storage: "denied",
+      ad_user_data: "denied",
+      analytics_storage: "denied",
+      ad_personalization: "denied",
+      personalization_storage: "denied",
+      functionality_storage: "denied",
+      security_storage: "denied",
+    };
+    localStorage.setItem("consentMode", JSON.stringify(data));
+    updateState({ showBanner: false });
   };
 
   useEffect(() => {
-    const val = localStorage.getItem("cookieConsent");
+    const val = localStorage.getItem("consentMode");
 
-    const isGranted = val === "true";
+    // const isGranted = val === "true";
 
-    if (isGranted) {
-      allConsentGranted();
-    }
+    // if (isGranted) {
+    //   allConsentGranted();
+    // }
 
     updateState({
-      cookieVal: isGranted,
       showBanner: val === null,
     });
   }, []);
 
   return (
     <>
-      <script
+      {/* <script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GMT_ID}`}
       ></script>
-      <script>{gtmScript}</script>
+      <script>{gtmScript}</script> */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {dataLayer.push(arguments);}
+          if (localStorage.getItem('consentMode') === null){
+            gtag( 'consent', 'default', {
+                    'ad_storage': 'denied',
+                    'ad_user_data': 'denied',
+                    'analytics_storage': 'denied',
+                    'ad_personalization': 'denied',
+                    'personalization_storage': 'denied',
+                    'functionality_storage': 'denied',
+                    'security_storage': 'denied',
+            });
+          } else {
+            gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')));
+          }
+          `,
+        }}
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer', "${process.env.NEXT_PUBLIC_GMT_ID}");`,
+        }}
+      ></script>
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GMT_ID}`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
       {state.showBanner && (
         <>
           <div className={styles.cover} onClick={(e) => e.preventDefault()}>
